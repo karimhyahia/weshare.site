1a5a97142ce55c7a4b3e8128cf83ad4bfae93ffa
Karim Yahia <74925139+karimhyahia@users.noreply.github.com>
2025-11-19 12:24:25 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@supabase/supabase-js']
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 });
